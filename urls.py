604f991719970b6d4855617aebea6afe32fd40fe
sahilshekhawat <sahilshekhawat01@gmail.com>
2014-01-18 00:48:52 +0530

    added user home's url

---
+++ urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('app.views',
+    # Examples:
+      url(r'^home/$', 'home', name='userhome'),
+    # url(r'^$', 'hackathon.views.home', name='home'),
+    # url(r'^hackathon/', include('hackathon.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
