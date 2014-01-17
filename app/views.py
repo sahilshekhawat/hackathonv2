11c075478c3c64344587afcba788a90abb8fa319
sahilshekhawat <sahilshekhawat01@gmail.com>
2014-01-18 00:47:21 +0530

    added home views

--- app/views.py
+++ app/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 from google.appengine.api import users
 from models import date
 from models import account
+import os
+import sys
+
+
+def home(request):
+    if user:   
+    filedir = sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+    user = account()
+    user_function = users.get_current_user()  
+    user.email = user_function.email()
+    user.username = user_function.nickname()
+    href = users.create_logout_url('/')
+    link = "logout"
+        return render(request, 'home.html', { 'href': href, 'link': link, 'email': user.email, 'username': user.username, 'filedir': filedir})
+
+
