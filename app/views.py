4e8c44841a1ed1477bbfab45e94ac1895d9623a6
sahilshekhawat <sahilshekhawat01@gmail.com>
2014-01-18 14:31:27 +0530

    profile page's databases done.

--- app/views.py
+++ app/views.py
@@ -1,31 +1,40 @@
-# Create your views here.
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from google.appengine.api import users
 import os
 import sys
-from models import UserData, Time, Profile, Post
+import models
 
 
 def user_home(request):
     user_function = users.get_current_user() 
-    filedir = sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))      
- 
+    filedir = sys.path.insert(0, os.path.abspath(os.path.dirname(__file__))) 
+    contributed_list = []
+    started_list = []
+    paid=0
+    a=models.UserData()
     if user_function:
         username = user_function.nickname()  
         email = user_function.email()
         href = users.create_logout_url('/home')
+
+        for i in a.query(a._properties['username'] == username):
+            contributed_list.append(i.contributed_to)
+        for j in a.query(a._properties['username'] == username):
+            started_list.append(i.started)
+        for k in a.query(a._properties['username'] == username):
+            paid=k.paid
 	
-        if (not UserData.query(
-                UserData.username == user_function.nickname(),
-                UserData.email == user_function.email()).get()):
-            query = UserData(username=user_function.nickname(), email=user_function.email())
+        if (not models.UserData.query(models.UserData.username == user_function.nickname(),models.UserData.email == user_function.email()).get()):
+            query = models.UserData(username=user_function.nickname(), email=user_function.email())
             query.put()
+        else:
+            pass
     else:
         username = user_function.nickname()
         email = user_function.email()
         href = users.create_login_url('/home')
-    return render(request, 'home.html', { 'href': href, 'email': email, 'username': username, 'filedir': filedir})
+    return render(request, 'home.html', { 'href': href, 'email': email, 'username': username, 'filedir': filedir, 'contributed_list': contributed_list, 'started_list':started_list, 'paid': paid})
 
 
 
